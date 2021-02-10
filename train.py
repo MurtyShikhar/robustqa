99cdec2c52d7b8c44e4d91f61c634be44e797c59
MurtyShikhar <shikhar.murty@gmail.com>
2021-02-10 14:51:10 -0800

    less RAM

--- train.py
+++ train.py
@@ -33,19 +33,12 @@ def prepare_eval_data(dataset_dict, tokenizer):
     # For evaluation, we will need to convert our predictions to substrings of the context, so we keep the
     # corresponding example_id and we will store the offset mappings.
     tokenized_examples["id"] = []
-    tokenized_examples['context'] = []
-    tokenized_examples['question'] = []
-    tokenized_examples['answer'] = []
-
     for i in range(len(tokenized_examples["input_ids"])):
         # Grab the sequence corresponding to that example (to know what is the context and what is the question).
         sequence_ids = tokenized_examples.sequence_ids(i)
         # One example can give several spans, this is the index of the example containing this span of text.
         sample_index = sample_mapping[i]
         tokenized_examples["id"].append(dataset_dict["id"][sample_index])
-        tokenized_examples["context"].append(dataset_dict["context"][sample_index])
-        tokenized_examples["question"].append(dataset_dict["question"][sample_index])
-        tokenized_examples["answer"].append(dataset_dict["answer"][sample_index])
         # Set to None the offset_mapping that are not part of the context so it's easy to determine if a token
         # position is part of the context or not.
         tokenized_examples["offset_mapping"][i] = [
@@ -73,9 +66,6 @@ def prepare_train_data(dataset_dict, tokenizer):
     tokenized_examples["start_positions"] = []
     tokenized_examples["end_positions"] = []
     tokenized_examples['id'] = []
-    tokenized_examples['context'] = []
-    tokenized_examples['question'] = []
-    tokenized_examples['answer'] = []
     for i, offsets in enumerate(offset_mapping):
         # We will label impossible answers with the index of the CLS token.
         input_ids = tokenized_examples["input_ids"][i]
@@ -91,10 +81,6 @@ def prepare_train_data(dataset_dict, tokenizer):
         start_char = answer['answer_start'][0]
         end_char = start_char + len(answer['text'][0])
         tokenized_examples['id'].append(dataset_dict['id'][sample_index])
-        tokenized_examples['context'].append(dataset_dict['context'][sample_index])
-        tokenized_examples['question'].append(dataset_dict['question'][sample_index])
-        tokenized_examples['answer'].append(answer)
-
         # Start token index of the current span in the text.
         token_start_index = 0
         while sequence_ids[token_start_index] != 1:
@@ -119,7 +105,7 @@ def prepare_train_data(dataset_dict, tokenizer):
                 token_end_index -= 1
             tokenized_examples["end_positions"].append(token_end_index + 1)
             # assertion to check if this checks out
-            context = tokenized_examples['context'][-1]
+            context = dataset_dict['context'][sample_index]
             offset_st = offsets[tokenized_examples['start_positions'][-1]][0]
             offset_en = offsets[tokenized_examples['end_positions'][-1]][1]
             if context[offset_st : offset_en] != answer['text'][0]:
