25e685cd9e0778d4736ee24bdb082acbc210ed54
zhushen3128 <55868850+zhushen3128@users.noreply.github.com>
2021-03-01 20:08:00 -0800

    comment drop_empty_trans

--- backtranslate_sampling.py
+++ backtranslate_sampling.py
@@ -128,9 +128,9 @@ print('Sampled queries are being saved at:', args.sample_queries_dir)
 print('Sampled context are being saved at:', args.sample_context_dir)
 gold_answers = [dataset_dict['answer'][i] for i in sample_idx]
 
-[sample_idx, sample_context_individual_length, gold_answers] = drop_empty_trans(args.trans_queries_dir, args.trans_context_dir, sample_context_individual_length,
-                                                                              arg.dropped_queries_dir, args.dropped_queries_dir, 
-                                                                              list(sample_idx, sample_context_individual_length, gold_answers))
+#[sample_idx, sample_context_individual_length, gold_answers] = drop_empty_trans(args.trans_queries_dir, args.trans_context_dir, sample_context_individual_length,
+#                                                                              arg.dropped_queries_dir, args.dropped_queries_dir, 
+#                                                                              list(sample_idx, sample_context_individual_length, gold_answers))
 
 backtranslated_queries = concat_queries(args.backtranslate_queries_dir)
 backtranslated_context = concat_context(args.backtranslate_context_dir, sample_context_individual_length)
