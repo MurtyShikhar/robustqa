fde5402fe735f4ce85c65cf27cc1c55965587604
Danny Schwartz <deschwa2@illinois.edu>
2021-03-04 12:12:18 -0800

    Add feature function base class to establish pattern

---
+++ clustering/FeatureFunction.py
@@ -0,0 +1,9 @@
+
+class FeatureFunction:
+    """This is an abstract base class for an object that evaluates context paragraphs."""
+
+    def __init__(self):
+        pass
+
+    def evaluate(self, context: str) -> float:
+        raise NotImplementedError()
