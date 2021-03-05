Visualize KL divergence on each validation set, fix reporting bug

train.py
@@ -268,6 +268,7 @@ class Trainer():
         all_start_logits = []
         all_end_logits = []
         qa_loss_sum = 0.
+        kl_div_sum = 0.
         num_batches = 0
         with torch.no_grad(), \
                 tqdm(total=len(data_loader.dataset)) as progress_bar:
@@ -293,13 +294,13 @@ class Trainer():
                         kl_criterion = torch.nn.KLDivLoss(reduction="batchmean")
                         adv_loss = kl_criterion(discrim_log_prob, targets)
 
+                        kl_div_sum += adv_loss.item()
                         qa_loss_sum += self.adv_loss_weight * adv_loss.item()
 
                 else:
                     outputs = model(input_ids, attention_mask=attention_mask)
                     # Forward
                     start_logits, end_logits = outputs.start_logits, outputs.end_logits
-                # TODO: log information about adversarial network behavior?
 
                 all_start_logits.append(start_logits)
                 all_end_logits.append(end_logits)
@@ -316,7 +317,8 @@ class Trainer():
             results = util.eval_dicts(data_dict, preds)
             results_list = [('F1', results['F1']),
                             ('EM', results['EM']),
-                            ('Composite QA loss', qa_loss_sum/num_batches)]
+                            ('Composite QA loss', qa_loss_sum/num_batches),
+                            ('Avg KL Divergence', kl_div_sum/num_batches)]
         else:
             results_list = [('F1', -1.0),
                             ('EM', -1.0)]
@@ -425,13 +427,17 @@ class Trainer():
         results_str = ', '.join(f'{k}: {v:05.2f}' for k, v in curr_score.items())
 
         if report:
-            tune.report(EM=curr_score[val_set_name + ": EM"])
-            tune.report(F1=curr_score[val_set_name + ": F1"])
-            tune.report(QALoss=curr_score[val_set_name + ": Composite QA loss"])
+            tune.report(EM=curr_score["EM"])
+            tune.report(F1=curr_score["F1"])
+            tune.report(QALoss=curr_score["Composite QA loss"])
+            tune.report(KLDiv=curr_score["Avg KL Divergence"])
 
         self.log.info("Visualizing " + val_set_name + " metrics in TensorBoard...")
         for k, v in curr_score.items():
-            tbx.add_scalar(f'val/{k}', v, global_idx)
+            if val_set_name == "indomain_val":
+                tbx.add_scalar(f'indomain_val/{k}', v, global_idx)
+            else:
+                tbx.add_scalar(f'oodomain_val/{k}', v, global_idx)
 
         self.log.info(f'Eval {results_str}')
         if self.visualize_predictions: