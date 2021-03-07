4b26d86e530859376e315f5d68076353f77c15e6
zhushen3128 <55868850+zhushen3128@users.noreply.github.com>
2021-03-07 15:28:07 -0800

    Update nmt_model.py

--- nmt/nmt_model.py
+++ nmt/nmt_model.py
@@ -268,7 +268,7 @@ class NMT(nn.Module):
         for Y_t in torch.split(Y, 1, dim=0):
             Y_t = Y_t.squeeze(0)
             Ybar_t = torch.cat((Y_t, o_prev), dim=1)
-            next_value, o_t, e_t = self.step(Ybar_t, dec_state, enc_hiddens, enc_hiddens_proj, enc_masks)
+            dec_state, o_t, e_t = self.step(Ybar_t, dec_state, enc_hiddens, enc_hiddens_proj, enc_masks)
             combined_outputs.append(o_t)
             o_prev = o_t
 
