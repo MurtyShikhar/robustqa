301b823ebd4421eab6d0b0a3f1d1dc614aa5ef9a
Yijie Sun <58874075+yijiesun97@users.noreply.github.com>
2021-03-07 17:07:01 -0800

    Update args.py

--- args.py
+++ args.py
@@ -25,26 +25,26 @@ def get_train_test_args():
     parser.add_argument('--sample_prob', type=float, default=0.1)
     
     # where to save the sampling (with sampling prob) queries and context
-    parser.add_argument('--sample_queries_dir', type=str, default='2_layer/QA/sample_queries.txt')
-    parser.add_argument('--sample_context_dir', type=str, default='2_layer/QA/sample_context.txt')
+    parser.add_argument('--sample_queries_dir', type=str, default='2_layer_nmt/QA/sample_queries.txt')
+    parser.add_argument('--sample_context_dir', type=str, default='2_layer_nmt/QA/sample_context.txt')
     
     # where to retrieve the translated queries and context
-    parser.add_argument('--trans_queries_dir', type=str, default='2_layer/QA/trans_en_es_queries_beam_1.txt')
-    parser.add_argument('--trans_context_dir', type=str, default='2_layer/QA/trans_en_es_context_beam_1.txt')
+    parser.add_argument('--trans_queries_dir', type=str, default='2_layer_nmt/QA/trans_en_es_queries_beam_1.txt')
+    parser.add_argument('--trans_context_dir', type=str, default='2_layer_nmt/QA/trans_en_es_context_beam_1.txt')
     
     # where to store the blank line dropped translated queries and context
-    parser.add_argument('--dropped_queries_dir', type=str, default='2_layer/QA/trans_en_es_queries_beam_1_dropped.txt')
-    parser.add_argument('--dropped_context_dir', type=str, default='2_layer/QA/trans_en_es_context_beam_1_dropped.txt')
+    parser.add_argument('--dropped_queries_dir', type=str, default='2_layer_nmt/QA/trans_en_es_queries_beam_1_dropped.txt')
+    parser.add_argument('--dropped_context_dir', type=str, default='2_layer_nmt/QA/trans_en_es_context_beam_1_dropped.txt')
     
      # where to retrieve the back translated queries and context
-    parser.add_argument('--back_trans_queries_dir', type=str, default='2_layer/QA/trans_es_en_queries_beam_1.txt')
-    parser.add_argument('--back_trans_context_dir', type=str, default='2_layer/QA/trans_es_en_context_beam_1.txt')
+    parser.add_argument('--back_trans_queries_dir', type=str, default='2_layer_nmt/QA/trans_es_en_queries_beam_1.txt')
+    parser.add_argument('--back_trans_context_dir', type=str, default='2_layer_nmt/QA/trans_es_en_context_beam_1.txt')
     
     # where to store the blank line dropped back translated queries and context
-    parser.add_argument('--back_dropped_queries_dir', type=str, default='2_layer/QA/trans_es_en_queries_beam_1_dropped.txt')
-    parser.add_argument('--back_dropped_context_dir', type=str, default='2_layer/QA/trans_es_en_context_beam_1_dropped.txt')
+    parser.add_argument('--back_dropped_queries_dir', type=str, default='2_layer_nmt/QA/trans_es_en_queries_beam_1_dropped.txt')
+    parser.add_argument('--back_dropped_context_dir', type=str, default='2_layer_nmt/QA/trans_es_en_context_beam_1_dropped.txt')
     
-    parser.add_argument('--backtranslate_queries_dir', type=str, default='2_layer/QA/backtranslate_queries.txt')
-    parser.add_argument('--backtranslate_context_dir', type=str, default='2_layer/QA/backtranslate_context.txt')
+    parser.add_argument('--backtranslate_queries_dir', type=str, default='2_layer_nmt/QA/backtranslate_queries.txt')
+    parser.add_argument('--backtranslate_context_dir', type=str, default='2_layer_nmt/QA/backtranslate_context.txt')
     args = parser.parse_args()
     return args
