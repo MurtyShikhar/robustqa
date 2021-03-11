a35361d79983e4c3c5b26010b0958ca3eb2ed239
Yijie Sun <58874075+yijiesun97@users.noreply.github.com>
2021-03-10 20:54:02 -0800

    Update args.py

--- args.py
+++ args.py
@@ -27,7 +27,6 @@ def get_train_test_args():
     # where to save the sampling (with sampling prob) queries and context
     parser.add_argument('--sample_queries_dir', type=str, default='2_layer_nmt/QA/sample_queries.txt')
     parser.add_argument('--sample_context_dir', type=str, default='2_layer_nmt/QA/sample_context.txt')
-    parser.add_argument('--sample_paragraph_dir', type=str, default='2_layer_nmt/QA/sample_paragraph.txt')
     
     # where to retrieve the translated queries and context
     parser.add_argument('--trans_queries_dir', type=str, default='2_layer_nmt/QA/trans_en_es_queries_beam_5.txt')
@@ -44,6 +43,5 @@ def get_train_test_args():
     # where to store the blank line dropped back translated queries and context
     parser.add_argument('--back_dropped_queries_dir', type=str, default='2_layer_nmt/QA/trans_es_en_queries_beam_5_dropped.txt')
     parser.add_argument('--back_dropped_context_dir', type=str, default='2_layer_nmt/QA/trans_es_en_context_beam_5_dropped.txt')
-    parser.add_argument('--backtranslate_context_dir', type=str, default='2_layer_nmt/QA/backtranslate_context_beam_5.txt')
     args = parser.parse_args()
     return args
