3bfcdb086ab4ee65d0c7d95c4be3cb4068e5a1f4
zhushen3128 <55868850+zhushen3128@users.noreply.github.com>
2021-03-11 22:39:19 -0800

    Update google_translate.py

--- google_api/google_translate.py
+++ google_api/google_translate.py
@@ -5,20 +5,20 @@ from googletrans import Translator
 # pip install googletrans==3.1.0a0
 
 def en_es(input_dir, output_dir):
-    translator_en2es = Translator(dest = "es")
-    run(translator_en2es, input_dir, output_dir)
+    translator_en2es = Translator()
+    run(translator_en2es, input_dir, output_dir, src = "en", dest = "es")
 
     
 def es_en(input_dir, output_dir):
-    translator_es2en = Translator(dest = "en")
-    run(translator_es2en, input_dir, output_dir)
+    translator_es2en = Translator()
+    run(translator_es2en, input_dir, output_dir, src = "es", dest = "en")
 
-def run(model, input_dir, output_dir):
+def run(model, input_dir, output_dir, src, dest):
     in_file = open(input_dir, 'r')
     out_file = open(output_dir, 'w')
     
     for line in tqdm(in_file, desc='Translating'):
-        trans = model.translate(line).text
+        trans = model.translate(line, src = src, dest = dest).text
         out_file.write(trans + "\n")
 
     in_file.close()
