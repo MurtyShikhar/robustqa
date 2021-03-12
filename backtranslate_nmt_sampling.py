9196f26d479d64823635d8b9d3519d6d9afa4fe8
Yijie Sun <58874075+yijiesun97@users.noreply.github.com>
2021-03-12 02:21:46 -0800

    Update backtranslate_nmt_sampling.py

--- backtranslate_nmt_sampling.py
+++ backtranslate_nmt_sampling.py
@@ -16,19 +16,21 @@ def nmt_sampling(beam=1):
     keep_index_1 = get_keep_index(args.trans_queries_dir, args.trans_context_dir, sample_context_individual_length,
                               args.dropped_queries_dir, args.dropped_context_dir)
     sample_idx, dropped_context_individual_length, gold_answers, answer_locs = clean_lists(keep_index_1, [sample_idx, sample_context_individual_length, gold_answers, answer_locs])
-    print('Num of non-empty examples after translation:', len(keep_index_1))
+    print('Num of examples after translation:', len(keep_index_1))
 
     # back translation
     keep_index_2 = get_keep_index(args.back_trans_queries_dir, args.back_trans_context_dir, dropped_context_individual_length,
                               args.back_dropped_queries_dir, args.back_dropped_context_dir)
     sample_idx, dropped_context_individual_length, gold_answers, answer_locs = clean_lists(keep_index_2, [sample_idx, dropped_context_individual_length, gold_answers, answer_locs])
-    print('Num of non-empty examples after back translation:', len(keep_index_2))
+    print('Num of examples after back translation:', len(keep_index_2))
 
     # estimate new answers
     keep_index_3, new_answers = get_trans_context_answers(args.back_dropped_context_dir, dropped_context_individual_length, 
                                                           gold_answers, answer_locs, args.jaccard_threshold)
     drop_files(keep_index_3, args.back_dropped_queries_dir, args.back_dropped_context_dir,
                args.jaccard_queries_dir, args.jaccard_context_dir, dropped_context_individual_length)
+    sample_idx, dropped_context_individual_length, gold_answers, answer_locs = clean_lists(keep_index_3, [sample_idx, dropped_context_individual_length, gold_answers, answer_locs])
+    print('Num of examples after filtering by Jaccard similarity:', len(keep_index_3))
 
     # compute queries and context BLEU
     keep_index = [elem for idx, elem in enumerate(keep_index_1) if idx in [elem for idx, elem in enumerate(keep_index_2) if idx in keep_index_3]]
