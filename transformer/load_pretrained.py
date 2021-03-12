5c53e8c00990c5a7c27c7bf2288311c3f4ebfe28
Yijie Sun <58874075+yijiesun97@users.noreply.github.com>
2021-03-11 21:55:51 -0800

    Update load_pretrained.py

--- transformer/load_pretrained.py
+++ transformer/load_pretrained.py
@@ -5,22 +5,23 @@ from tqdm import tqdm
 # pip install hydra-core
 # pip install sacremoses
 
-def backtranslate(args):
+def en_de(input_dir, output_dir):
     en2de = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.en-de',
                        checkpoint_file='model1.pt:model2.pt:model3.pt:model4.pt',
                        tokenizer='moses', bpe='fastbpe')
     en2de.eval()
     en2de.cuda()
+    run(en2de, args.input_dir, args.trans_dir)
 
+    
+def de_en(args):
     de2en = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.de-en',
                        checkpoint_file='model1.pt:model2.pt:model3.pt:model4.pt',
                        tokenizer='moses', bpe='fastbpe')
     de2en.eval()
     de2en.cuda()
-
-    run(en2de, args.input_dir, args.trans_dir)
     run(de2en, args.trans_dir, args.backtrans_dir)
-
+    
 
 def run(model, input_dir, output_dir):
     in_file = open(input_dir, 'r')
@@ -36,14 +37,16 @@ def run(model, input_dir, output_dir):
 
 
 if __name__ == '__main__':
-    queries_args = get_queries_args()
-    #context_args = get_context_args()
-
     print("========= Translating queries =========")
-    backtranslate(queries_args)
-
-    #print("========= Translating context =========")
-    #backtranslate(context_args)
+    queries_args = get_queries_args()
+    en_de(queries_args.input_dir, queries_args.trans_dir)
+    de_en(queries_args.trans_dir, queries_args.backtrans_dir)
+    
+#     print("========= Translating context =========")
+#     context_args = get_context_args()
+#     backtranslate(context_args)
+#     en_de(context_args.input_dir, context_args.trans_dir)
+#     de_en(context_args.trans_dir, context_args.backtrans_dir)
 
 # Test
 # de_trans = en2de.translate('Hello world!')
