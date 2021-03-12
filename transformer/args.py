a85e88de98710295589cf512fd47aaaa0ab676c0
Yijie Sun <58874075+yijiesun97@users.noreply.github.com>
2021-03-11 22:01:52 -0800

    Update args.py

--- transformer/args.py
+++ transformer/args.py
@@ -1,18 +1,18 @@
 import argparse
 
-def get_queries_args():
+def get_queries_args(lang='de'):
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_dir', type=str, default='../2_layer_nmt/QA/sample_queries.txt')
-    parser.add_argument('--trans_dir', type=str, default='trans_en_de_queries.txt')
-    parser.add_argument('--backtrans_dir', type=str, default='trans_de_en_queries.txt')
+    parser.add_argument('--trans_dir', type=str, default='trans_en_{0}_queries.txt'.format(lang))
+    parser.add_argument('--backtrans_dir', type=str, default='trans_{0}_en_queries.txt'.format(lang))
     args = parser.parse_args()
     return args
 
-def get_context_args():
+def get_context_args(lang='de'):
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_dir', type=str, default='../2_layer_nmt/QA/sample_context.txt')
-    parser.add_argument('--trans_dir', type=str, default='trans_en_de_context.txt')
-    parser.add_argument('--backtrans_dir', type=str, default='trans_de_en_context.txt')
+    parser.add_argument('--trans_dir', type=str, default='trans_en_{0}_context.txt'.format(lang))
+    parser.add_argument('--backtrans_dir', type=str, default='trans_{0}_en_context.txt'.format(lang))
     args = parser.parse_args()
     return args
 
