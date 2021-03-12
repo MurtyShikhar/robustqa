51ce9e9b0111e71f04118b59d4d2ef1702d82050
Yijie Sun <58874075+yijiesun97@users.noreply.github.com>
2021-03-11 22:11:21 -0800

    Update load_pretrained.py

--- transformer/load_pretrained.py
+++ transformer/load_pretrained.py
@@ -7,8 +7,7 @@ from tqdm import tqdm
 
 def en_de(input_dir, output_dir):
     en2de = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.en-de',
-                       checkpoint_file='model1.pt:model2.pt:model3.pt:model4.pt',
-                       tokenizer='moses', bpe='fastbpe')
+                       checkpoint_file='model1.pt', tokenizer='moses', bpe='fastbpe')
     en2de.eval()
     en2de.cuda()
     run(en2de, input_dir, output_dir)
@@ -16,13 +15,28 @@ def en_de(input_dir, output_dir):
     
 def de_en(input_dir, output_dir):
     de2en = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.de-en',
-                       checkpoint_file='model1.pt:model2.pt:model3.pt:model4.pt',
-                       tokenizer='moses', bpe='fastbpe')
+                       checkpoint_file='model1.pt', tokenizer='moses', bpe='fastbpe')
     de2en.eval()
     de2en.cuda()
     run(de2en, input_dir, output_dir)
     
 
+def en_ru(input_dir, output_dir):
+    en2de = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.en-ru',
+                       checkpoint_file='model1.pt', tokenizer='moses', bpe='fastbpe')
+    en2ru.eval()
+    en2ru.cuda()
+    run(en2ru, input_dir, output_dir)
+    
+    
+def ru_en(input_dir, output_dir):
+    ru2en = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.ru-en',
+                       checkpoint_file='model1', tokenizer='moses', bpe='fastbpe')
+    ru2en.eval()
+    ru2en.cuda()
+    run(ru2en, input_dir, output_dir)
+    
+    
 def run(model, input_dir, output_dir):
     in_file = open(input_dir, 'r')
     out_file = open(output_dir, 'w')
@@ -37,14 +51,23 @@ def run(model, input_dir, output_dir):
 
 
 if __name__ == '__main__':
-    print("========= Translating queries =========")
+    print("========= Translating queries (de) =========")
     queries_args = get_queries_args(lang='de')
     en_de(queries_args.input_dir, queries_args.trans_dir)
     de_en(queries_args.trans_dir, queries_args.backtrans_dir)
     
-#     print("========= Translating context =========")
+#     print("========= Translating context (de) =========")
 #     context_args = get_context_args(lang='de')
-#     backtranslate(context_args)
+#     en_de(context_args.input_dir, context_args.trans_dir)
+#     de_en(context_args.trans_dir, context_args.backtrans_dir)
+
+#     print("========= Translating queries (ru) =========")
+#     queries_args = get_queries_args(lang='ru')
+#     en_de(queries_args.input_dir, queries_args.trans_dir)
+#     de_en(queries_args.trans_dir, queries_args.backtrans_dir)
+    
+#     print("========= Translating context (ru) =========")
+#     context_args = get_context_args(lang='ru')
 #     en_de(context_args.input_dir, context_args.trans_dir)
 #     de_en(context_args.trans_dir, context_args.backtrans_dir)
 
