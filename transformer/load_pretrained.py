3a6d07dcc591d8563b640dedd9d998b8adecbb5d
Yijie Sun <58874075+yijiesun97@users.noreply.github.com>
2021-03-12 14:39:35 -0800

    Update load_pretrained.py

--- transformer/load_pretrained.py
+++ transformer/load_pretrained.py
@@ -1,5 +1,5 @@
 import torch
-from args import get_queries_args, get_context_args
+from transformer_args import get_queries_args, get_context_args
 from tqdm import tqdm
 # pip install Cython
 # pip install hydra-core
