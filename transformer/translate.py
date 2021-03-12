a15a777e9e7dc618587b0e173467e3c38b27ceed
Yijie Sun <58874075+yijiesun97@users.noreply.github.com>
2021-03-11 22:24:58 -0800

    Create translate.py

---
+++ transformer/translate.py
@@ -0,0 +1,48 @@
+import torch
+from args import get_queries_args, get_context_args
+from tqdm import tqdm
+# pip install Cython
+# pip install hydra-core
+# pip install sacremoses
+
+def transformer_19(src, tgt, input_dir, output_dir):
+    model = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.{0}-{1}'.format(src, tgt),
+                       checkpoint_file='model1.pt', tokenizer='moses', bpe='fastbpe')
+    model.eval()
+    model.cuda()
+    run(model, input_dir, output_dir)
+
+    
+def run(model, input_dir, output_dir):
+    in_file = open(input_dir, 'r')
+    out_file = open(output_dir, 'w')
+
+    for line in tqdm(in_file, desc='Translating'):
+        trans = model.translate(line)
+        out_file.write(trans + "\n")
+
+    in_file.close()
+    out_file.close()
+    print("Translations stored at:", output_dir)
+
+
+if __name__ == '__main__':
+    print("========= Translating queries (de) =========")
+    queries_args = get_queries_args(lang='de')
+    transformer_19('en', 'de', queries_args.input_dir, queries_args.trans_dir)
+    transformer_19('de', 'en', queries_args.trans_dir, queries_args.backtrans_dir)
+    
+#     print("========= Translating context (de) =========")
+#     context_args = get_context_args(lang='de')
+#     transformer_19('en', 'de', context_args.input_dir, context_args.trans_dir)
+#     transformer_19('de', 'en', context_args.trans_dir, context_args.backtrans_dir)
+
+#     print("========= Translating queries (ru) =========")
+#     queries_args = get_queries_args(lang='ru')
+#     transformer_19('en', 'ru', queries_args.input_dir, queries_args.trans_dir)
+#     transformer_19('ru', 'en', queries_args.trans_dir, queries_args.backtrans_dir)
+    
+#     print("========= Translating context (ru) =========")
+#     context_args = get_context_args(lang='ru')
+#     transformer_19('en', 'ru', context_args.input_dir, context_args.trans_dir)
+#     transformer_19('ru', 'en', context_args.trans_dir, context_args.backtrans_dir)
