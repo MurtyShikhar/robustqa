dbc6b78d934a0f89b08b75a8714247f78a7e3f30
Yijie Sun <58874075+yijiesun97@users.noreply.github.com>
2021-03-12 23:53:04 -0800

    Update load_pretrained.py

--- transformer/load_pretrained.py
+++ transformer/load_pretrained.py
@@ -42,7 +42,7 @@ def run(model, input_dir, output_dir, src_tgt):
     in_file = open(input_dir, 'r').readlines()
     out_file = open(output_dir, 'w')
    
-    with tqdm(len(in_file), desc='Translating: ' + src_tgt):
+    with tqdm(total=len(in_file), desc='Translating: ' + src_tgt) as progress_bar:
         for line in in_file:
             trans = model.translate(line)
             out_file.write(trans + "\n")
