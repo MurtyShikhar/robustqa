d96be73325642e1f564fd026459c72aac51ab7e4
zhushen3128 <55868850+zhushen3128@users.noreply.github.com>
2021-03-13 15:28:28 -0800

    Update load_pretrained.py

--- transformer/load_pretrained.py
+++ transformer/load_pretrained.py
@@ -39,11 +39,16 @@ def ru_en(input_dir, output_dir):
     
     
 def run(model, input_dir, output_dir, src_tgt):
-    in_file = open(input_dir, 'r').readlines()
+    #in_file = open(input_dir, 'r').readlines()
+    in_file = open(input_dir, 'r')
     out_file = open(output_dir, 'w')
    
     #with tqdm(total=len(in_file), desc='Translating: ' + src_tgt) as progress_bar:
-    for line in tqdm(in_file, desc='Translating: ' + src_tgt):
+    #for line in tqdm(in_file, desc='Translating: ' + src_tgt):
+    #    trans = model.translate(line)
+    #    out_file.write(trans + "\n")
+        
+    for line in tqdm(in_file.readlines(), desc='Translating: ' + src_tgt):
         trans = model.translate(line)
         out_file.write(trans + "\n")
 
