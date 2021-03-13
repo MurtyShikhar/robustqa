607eb94bfe678e30533f632de5fb5c795d4cb8ac
zhushen3128 <55868850+zhushen3128@users.noreply.github.com>
2021-03-12 18:50:19 -0800

    Update backtranslate_util.py

--- backtranslate_util.py
+++ backtranslate_util.py
@@ -161,8 +161,6 @@ def get_trans_context_answers(context_dir, sample_context_individual_length,
             new_answers: list of new_answers
     """
     in_file = open(context_dir, 'r')
-    # for sanity check 
-    out_file = open('2_layer_nmt/QA/jaccard_similarity_beam_1.txt', 'w')
     
     num_samples = len(sample_context_individual_length)
     keep_index = []
@@ -177,10 +175,7 @@ def get_trans_context_answers(context_dir, sample_context_individual_length,
         jac_scores = []
         char_count = 0
         
-        out_file.write("No. Sample : " + str(i) + "\n")
-
         for j in range(sample_context_individual_length[i]):
-          out_file.write("No. Sentence in Sample Context : " + str(j) + "\n")
           context_sent = in_file.readline().strip()
           
           for k in range(len(curr_locs)):
@@ -190,13 +185,6 @@ def get_trans_context_answers(context_dir, sample_context_individual_length,
               new_curr_answers.append(best_substring)
               jac_scores.append(best_jac_score)
               
-              # for sanity check
-              out_file.write("Context Sentence: " + context_sent + "\n")
-              out_file.write("Gold Answer: " + curr_answers[k] + "\n")
-              out_file.write("Estimated Answer: " + best_substring + "\n")
-              out_file.write("Jaccard Score: " + str(best_jac_score) + "\n")
-              out_file.write("\n")
-            
           char_count += len(context_sent + " ")
         
         if len(jac_scores) > 0 and max(jac_scores) > threshold:
@@ -204,7 +192,6 @@ def get_trans_context_answers(context_dir, sample_context_individual_length,
          new_answers.append(dict({'answer_start': new_start_idx, 'text': new_curr_answers}))
     
     in_file.close()
-    out_file.close()
     return keep_index, new_answers
   
   
