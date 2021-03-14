ee0d54b54e9c48a5108cc129e25c43e0df0cfdca
jamesli1997 <jamesli1997@gmail.com>
2021-03-13 22:19:32 -0800

    added new translate method

--- google_api/google_translate_new.py
+++ google_api/google_translate_new.py
@@ -1,30 +1,19 @@
 import torch
 from google_args import get_google_args
 from tqdm import tqdm
-from googletrans import Translator
+from google_trans_new import google_translator
 import time
 # pip install googletrans==3.1.0a0
-
-def en_es(input_dir, output_dir):
-    translator_en2es = Translator()
-    run(translator_en2es, input_dir, output_dir, src = "en", dest = "es")
-
-    
-def es_en(input_dir, output_dir):
-    translator_es2en = Translator()
-    run(translator_es2en, input_dir, output_dir, src = "es", dest = "en")
 
 def run(input_dir, output_dir, src, dest):
     in_file = open(input_dir, 'r')
     out_file = open(output_dir, 'w')
-    model = Translator(service_urls=['translate.googleapis.com'])
+    model = google_translator()
 
     i = 0
     for line in tqdm(in_file.readlines(), desc='Translating'):
-        if i % 100 == 0:
-            time.sleep(0.5)
-            model = Translator(service_urls=['translate.googleapis.com'])
-        trans = model.translate(line, src = src, dest = dest).text
+        trans = model.translate(line, lang_src = src, lang_tgt = dest)
+        time.sleep(0.1)
         i += 1
         out_file.write(trans + "\n")
 
