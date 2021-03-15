03df775bea969b1c2c32ee224e5cba17878290bd
Yijie Sun <58874075+yijiesun97@users.noreply.github.com>
2021-03-14 17:32:47 -0700

    Update args.py

--- args.py
+++ args.py
@@ -33,16 +33,11 @@ def get_train_test_args():
     args = parser.parse_args()
     return args
     
-def get_nmt_args(beam=1, indomain=True):
+def get_nmt_args(beam=1):
     parser = argparse.ArgumentParser()
-    if indomain:
-        parser.add_argument('--train-datasets', type=str, default='squad,nat_questions,newsqa')
-        parser.add_argument('--train-dir', type=str, default='datasets/indomain_train')
-        parser.add_argument('--sample_prob', type=float, default=0.1)
-    else:
-        parser.add_argument('--train-datasets', type=str, default='race,relation_extraction,duorc')
-        parser.add_argument('--train-dir', type=str, default='datasets/oodomain_train')
-        parser.add_argument('--sample_prob', type=float, default=1.0)
+    parser.add_argument('--train-datasets', type=str, default='squad,nat_questions,newsqa')
+    parser.add_argument('--train-dir', type=str, default='datasets/indomain_train')
+    parser.add_argument('--sample_prob', type=float, default=0.1)
     parser.add_argument('--seed', type=int, default=42)
         
     # where to save the sampling (with sampling prob) queries and context
@@ -83,16 +78,56 @@ def get_nmt_args(beam=1, indomain=True):
     args = parser.parse_args()
     return args
 
-def get_transformer_args(lang='de', indomain=True):
+def get_nmt_ood_args(beam=1):
     parser = argparse.ArgumentParser()
-    if indomain:
-        parser.add_argument('--train-datasets', type=str, default='squad,nat_questions,newsqa')
-        parser.add_argument('--train-dir', type=str, default='datasets/indomain_train')
-        parser.add_argument('--sample_prob', type=float, default=0.1)
-    else:
-        parser.add_argument('--train-datasets', type=str, default='race,relation_extraction,duorc')
-        parser.add_argument('--train-dir', type=str, default='datasets/oodomain_train')
-        parser.add_argument('--sample_prob', type=float, default=1.0)
+    parser.add_argument('--train-datasets', type=str, default='race,relation_extraction,duorc')
+    parser.add_argument('--train-dir', type=str, default='datasets/oodomain_train')
+    parser.add_argument('--sample_prob', type=float, default=1.0)
+    parser.add_argument('--seed', type=int, default=42)
+        
+    # where to save the sampling (with sampling prob) queries and context
+    parser.add_argument('--sample_queries_dir', type=str, default='2_layer_nmt/QA/ood/sample_queries.txt')
+    parser.add_argument('--sample_context_dir', type=str, default='2_layer_nmt/QA/ood/sample_context.txt')
+    
+    # where to retrieve the translated queries and context
+    parser.add_argument('--trans_queries_dir', type=str, default='2_layer_nmt/QA/ood/trans_en_es_queries_beam_{0}.txt'.format(beam))
+    parser.add_argument('--trans_context_dir', type=str, default='2_layer_nmt/QA/ood/trans_en_es_context_beam_{0}.txt'.format(beam))
+    
+    # where to store the blank line dropped translated queries and context
+    parser.add_argument('--dropped_queries_dir', type=str, default='2_layer_nmt/QA/ood/trans_en_es_queries_beam_{0}_dropped.txt'.format(beam))
+    parser.add_argument('--dropped_context_dir', type=str, default='2_layer_nmt/QA/ood/trans_en_es_context_beam_{0}_dropped.txt'.format(beam))
+
+     # where to retrieve the back translated queries and context
+    parser.add_argument('--back_trans_queries_dir', type=str, default='2_layer_nmt/QA/ood/trans_es_en_queries_beam_{0}.txt'.format(beam))
+    parser.add_argument('--back_trans_context_dir', type=str, default='2_layer_nmt/QA/ood/trans_es_en_context_beam_{0}.txt'.format(beam))
+    
+    # where to store the blank line dropped back translated queries and context
+    parser.add_argument('--back_dropped_queries_dir', type=str, default='2_layer_nmt/QA/ood/trans_es_en_queries_beam_{0}_dropped.txt'.format(beam))
+    parser.add_argument('--back_dropped_context_dir', type=str, default='2_layer_nmt/QA/ood/trans_es_en_context_beam_{0}_dropped.txt'.format(beam))
+    
+    # jaccard similarity filtering
+    parser.add_argument('--jaccard_threshold', type=float, default=0.65)
+    parser.add_argument('--jaccard_queries_dir', type=str, default='2_layer_nmt/QA/ood/trans_es_en_queries_beam_{0}_jaccard.txt'.format(beam))
+    parser.add_argument('--jaccard_context_dir', type=str, default='2_layer_nmt/QA/ood/trans_es_en_context_beam_{0}_jaccard.txt'.format(beam))
+    
+    # where to store dropped sample files
+    parser.add_argument('--sample_queries_dropped_dir', type=str, default='2_layer_nmt/QA/ood/sample_queries_beam_{0}_dropped.txt'.format(beam))
+    parser.add_argument('--sample_context_dropped_dir', type=str, default='2_layer_nmt/QA/ood/sample_context_beam_{0}_dropped.txt'.format(beam))
+    
+    # where to store augmented dataset
+    parser.add_argument('--aug_dataset_dict', type=str, default='augmented_dataset_beam_{0}_ood.json'.format(beam))
+    parser.add_argument('--aug_dataset_pickle', type=str, default='augmented_dataset_beam_{0}_ood.pickle'.format(beam))
+
+    # whether add backtranslated data to finetune
+    parser.add_argument('--train_with_backtranslate', type=bool, default=True)
+    args = parser.parse_args()
+    return args
+
+def get_transformer_args(lang='de'):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train-datasets', type=str, default='squad,nat_questions,newsqa')
+    parser.add_argument('--train-dir', type=str, default='datasets/indomain_train')
+    parser.add_argument('--sample_prob', type=float, default=0.1)
     parser.add_argument('--seed', type=int, default=42)
     
     # where to save the sampling (with sampling prob) queries and context
@@ -115,10 +150,42 @@ def get_transformer_args(lang='de', indomain=True):
     # where to store augmented dataset
     parser.add_argument('--aug_dataset_dict', type=str, default='augmented_dataset_transformer_{0}.json'.format(lang))
     parser.add_argument('--aug_dataset_pickle', type=str, default='augmented_dataset_transformer_{0}.pickle'.format(lang))
-
+    
     # whether add backtranslated data to finetune
-#     parser.add_argument('--train_with_backtranslate', type=bool, default=True)  
+    parser.add_argument('--train_with_backtranslate', type=bool, default=True)
+    args = parser.parse_args()
+    return args
 
+def get_transformer_ood_args(lang='de'):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train-datasets', type=str, default='race,relation_extraction,duorc')
+    parser.add_argument('--train-dir', type=str, default='datasets/oodomain_train')
+    parser.add_argument('--sample_prob', type=float, default=1.0)
+    parser.add_argument('--seed', type=int, default=42)
+    
+    # where to save the sampling (with sampling prob) queries and context
+    parser.add_argument('--sample_queries_dir', type=str, default='transformer/QA/ood/sample_queries.txt')
+    parser.add_argument('--sample_context_dir', type=str, default='transformer/QA/ood/sample_context.txt')
+    
+    # where to retrieve the back translated queries and context
+    parser.add_argument('--back_trans_queries_dir', type=str, default='transformer/QA/ood/trans_{0}_en_queries.txt'.format(lang))
+    parser.add_argument('--back_trans_context_dir', type=str, default='transformer/QA/ood/trans_{0}_en_context.txt'.format(lang))
+
+     # jaccard similarity filtering
+    parser.add_argument('--jaccard_threshold', type=float, default=0.65)
+    parser.add_argument('--jaccard_queries_dir', type=str, default='transformer/QA/ood/trans_{0}_en_queries_jaccard.txt'.format(lang))
+    parser.add_argument('--jaccard_context_dir', type=str, default='transformer/QA/ood/trans_{0}_en_context_jaccard.txt'.format(lang))
+    
+    # where to store dropped sample files
+    parser.add_argument('--sample_queries_dropped_dir', type=str, default='transformer/QA/ood/sample_queries_{0}_dropped.txt'.format(lang))
+    parser.add_argument('--sample_context_dropped_dir', type=str, default='transformer/QA/ood/sample_context_{0}_dropped.txt'.format(lang))
+    
+    # where to store augmented dataset
+    parser.add_argument('--aug_dataset_dict', type=str, default='augmented_dataset_transformer_{0}_ood.json'.format(lang))
+    parser.add_argument('--aug_dataset_pickle', type=str, default='augmented_dataset_transformer_{0}_ood.pickle'.format(lang))
+    
+    # whether add backtranslated data to finetune
+    parser.add_argument('--train_with_backtranslate', type=bool, default=True)
     args = parser.parse_args()
     return args
 
