b8100fe681159201a75b3d2b49d62197e99e48c7
zhushen3128 <55868850+zhushen3128@users.noreply.github.com>
2021-03-14 18:28:23 -0700

    Update transformer_args.py

--- transformer/transformer_args.py
+++ transformer/transformer_args.py
@@ -12,3 +12,17 @@ def get_transformer_args(lang='de'):
     
     args = parser.parse_args()
     return args
+
+
+def get_transformer_ood_args(lang='de'):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--queries_input_dir', type=str, default='QA/ood/sample_queries.txt')
+    parser.add_argument('--queries_trans_dir', type=str, default='QA/ood/trans_en_{0}_queries.txt'.format(lang))
+    parser.add_argument('--queries_backtrans_dir', type=str, default='QA/ood/trans_{0}_en_queries.txt'.format(lang))
+    
+    parser.add_argument('--context_input_dir', type=str, default='QA/ood/sample_context.txt')
+    parser.add_argument('--context_trans_dir', type=str, default='QA/ood/trans_en_{0}_context.txt'.format(lang))
+    parser.add_argument('--context_backtrans_dir', type=str, default='QA/ood/trans_{0}_en_context.txt'.format(lang))
+    
+    args = parser.parse_args()
+    return args
