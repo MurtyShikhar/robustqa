2e12d36375055bf3dd9123451aa1ce9241ce1711
zhushen3128 <55868850+zhushen3128@users.noreply.github.com>
2021-03-14 21:09:44 -0700

    add ood case

--- transformer/transformer_args.py
+++ transformer/transformer_args.py
@@ -1,28 +1,25 @@
 import argparse
 
-def get_transformer_args(lang='de'):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--queries_input_dir', type=str, default='QA/sample_queries.txt')
-    parser.add_argument('--queries_trans_dir', type=str, default='QA/trans_en_{0}_queries.txt'.format(lang))
-    parser.add_argument('--queries_backtrans_dir', type=str, default='QA/trans_{0}_en_queries.txt'.format(lang))
+def get_transformer_args(lang='de', indomain=True):
+    if indomain: 
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--queries_input_dir', type=str, default='QA/sample_queries.txt')
+        parser.add_argument('--queries_trans_dir', type=str, default='QA/trans_en_{0}_queries.txt'.format(lang))
+        parser.add_argument('--queries_backtrans_dir', type=str, default='QA/trans_{0}_en_queries.txt'.format(lang))
     
-    parser.add_argument('--context_input_dir', type=str, default='QA/sample_context.txt')
-    parser.add_argument('--context_trans_dir', type=str, default='QA/trans_en_{0}_context.txt'.format(lang))
-    parser.add_argument('--context_backtrans_dir', type=str, default='QA/trans_{0}_en_context.txt'.format(lang))
+        parser.add_argument('--context_input_dir', type=str, default='QA/sample_context.txt')
+        parser.add_argument('--context_trans_dir', type=str, default='QA/trans_en_{0}_context.txt'.format(lang))
+        parser.add_argument('--context_backtrans_dir', type=str, default='QA/trans_{0}_en_context.txt'.format(lang))
     
-    args = parser.parse_args()
-    return args
-
-
-def get_transformer_ood_args(lang='de'):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--queries_input_dir', type=str, default='QA/ood/sample_queries.txt')
-    parser.add_argument('--queries_trans_dir', type=str, default='QA/ood/trans_en_{0}_queries.txt'.format(lang))
-    parser.add_argument('--queries_backtrans_dir', type=str, default='QA/ood/trans_{0}_en_queries.txt'.format(lang))
+    else: 
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--queries_input_dir', type=str, default='QA/ood/sample_queries.txt')
+        parser.add_argument('--queries_trans_dir', type=str, default='QA/ood/trans_en_{0}_queries.txt'.format(lang))
+        parser.add_argument('--queries_backtrans_dir', type=str, default='QA/ood/trans_{0}_en_queries.txt'.format(lang))
     
-    parser.add_argument('--context_input_dir', type=str, default='QA/ood/sample_context.txt')
-    parser.add_argument('--context_trans_dir', type=str, default='QA/ood/trans_en_{0}_context.txt'.format(lang))
-    parser.add_argument('--context_backtrans_dir', type=str, default='QA/ood/trans_{0}_en_context.txt'.format(lang))
+        parser.add_argument('--context_input_dir', type=str, default='QA/ood/sample_context.txt')
+        parser.add_argument('--context_trans_dir', type=str, default='QA/ood/trans_en_{0}_context.txt'.format(lang))
+        parser.add_argument('--context_backtrans_dir', type=str, default='QA/ood/trans_{0}_en_context.txt'.format(lang))
     
     args = parser.parse_args()
     return args
