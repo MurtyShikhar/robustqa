7c0b10f73c25a4a1cdc70de09a2444ff63d2d645
zhushen3128 <55868850+zhushen3128@users.noreply.github.com>
2021-03-14 21:14:10 -0700

    add ood case

--- transformer/transformer_args.py
+++ transformer/transformer_args.py
@@ -1,8 +1,10 @@
 import argparse
 
 def get_transformer_args(lang='de', indomain=True):
+    
+    parser = argparse.ArgumentParser()
+    
     if indomain: 
-        parser = argparse.ArgumentParser()
         parser.add_argument('--queries_input_dir', type=str, default='QA/sample_queries.txt')
         parser.add_argument('--queries_trans_dir', type=str, default='QA/trans_en_{0}_queries.txt'.format(lang))
         parser.add_argument('--queries_backtrans_dir', type=str, default='QA/trans_{0}_en_queries.txt'.format(lang))
@@ -12,7 +14,6 @@ def get_transformer_args(lang='de', indomain=True):
         parser.add_argument('--context_backtrans_dir', type=str, default='QA/trans_{0}_en_context.txt'.format(lang))
     
     else: 
-        parser = argparse.ArgumentParser()
         parser.add_argument('--queries_input_dir', type=str, default='QA/ood/sample_queries.txt')
         parser.add_argument('--queries_trans_dir', type=str, default='QA/ood/trans_en_{0}_queries.txt'.format(lang))
         parser.add_argument('--queries_backtrans_dir', type=str, default='QA/ood/trans_{0}_en_queries.txt'.format(lang))
